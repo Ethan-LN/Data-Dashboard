6463ac5052ef56e6e82256a718576295d3acea0f
Ethan <et.l.nicholas@gmail.com>
2023-08-08 20:42:38 +1000

    JenkinFile 12

--- Build.sh
+++ Build.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 cd /var/lib/jenkins/workspace/react-cicd/
 echo"Upgrade the npm and node,  currently"
+
+# Check if the line is already present in the file
+if ! grep -q "export PATH=/usr/local/bin:\$PATH" ~/.bashrc; then
+    echo "export PATH=/usr/local/bin:\$PATH" | sudo tee -a ~/.bashrc
+fi
 node -v
+echo "Before uprde "
 npm -v
 sudo npm cache clean -f
 sudo npm install -g n
