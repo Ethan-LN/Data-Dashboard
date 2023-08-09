9a8b10dc094731acb72a13723b3f7bd04140a092
Ethan <et.l.nicholas@gmail.com>
2023-08-09 16:02:55 +1000

    setup the area of nav and dashboard grids

---
+++ src/components/Dashboard.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "../styles/Dashboard.css";
+
+const Dashboard = () => {
+  return (
+    <div className="dashboard__container">
+      <div className="dashboard">
+        <div class="parent">
+          <div class="div1"> div1</div>
+          <div class="div2"> div2</div>
+          <div class="div3"> div3</div>
+          <div class="div4"> div4</div>
+          <div class="div5"> div5</div>
+          <div class="div6"> div6</div>
+          <div class="div7"> div7</div>
+          <div class="div8"> div8</div>
+          <div class="div9"> div9</div>
+          <div class="div10"> div10</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
---
+++ src/components/Navigation.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "../styles/Navigation.css"
+
+const Navigation = () => {
+  return (
+    <div className='nav'>Navigation</div>
+  )
+}
+
+export default Navigation
