7e30cd961c91b0bec32710d729031e016c8c420b
Ethan <et.l.nicholas@gmail.com>
2023-08-09 23:41:41 +1000

    add Links to Navbar and pages

--- src/components/ManageSection.js
+++ src/components/ManageSection.js
@@ -1,29 +1,51 @@
 import React from "react";
 import "../styles/ManageSection.css";
 import { TableDisplay } from "./TableDisplay";
+import { Link } from "react-router-dom";
+import { ManageStaff } from "../pages/ManageStaff/ManageStaff";
 
 export const ManageSection = () => {
-  
-    const DisplayTable = () => {
-    return <div><TableDisplay /></div>;
+  const DisplayTable = () => {
+    return (
+      <div>
+        <ManageStaff />
+      </div>
+    );
   };
 
   return (
     <div className="section">
       <div className="data__nav">Data </div>
       <div className="btns__group">
-        <button onClick={DisplayTable}>Manage Staff</button>
-        <button>CustomerContact</button>
-        <button>Invoice Balance</button>
+        <Link to="/staff">
+          <button>Manage Staff</button>
+        </Link>
+        <Link to="/customer">
+          {" "}
+          <button>CustomerContact</button>
+        </Link>
+        <Link to="/invoice">
+          <button>Invoice Balance</button>
+        </Link>
       </div>
 
       <div className="charts__nav">Charts </div>
       <div className="btns__group">
-        <button>Line Chart</button>
-        <button>Bar Chart</button>
-        <button>Pie Chart</button>
-        <button>Area Chart</button>
-        <button>Map Chart</button>
+        <Link to="/line">
+          <button>Line Chart</button>
+        </Link>
+        <Link to="/bar">
+          <button>Bar Chart</button>
+        </Link>
+        <Link to="/pie">
+          <button>Pie Chart</button>
+        </Link>
+        <Link to="/area">
+          <button>Area Chart</button>
+        </Link>
+        <Link to="/map">
+          <button>Map Chart</button>
+        </Link>
       </div>
     </div>
   );
