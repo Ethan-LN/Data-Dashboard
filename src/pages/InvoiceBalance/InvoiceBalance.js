0004a4d885225945c70dac9bf97ffd371a6f92a6
Ethan <et.l.nicholas@gmail.com>
2023-08-10 13:35:25 +1000

    add charts and table template to the app

--- src/pages/InvoiceBalance/InvoiceBalance.js
+++ src/pages/InvoiceBalance/InvoiceBalance.js
@@ -6,7 +6,8 @@ export const InvoiceBalance = () => {
   return (
     <div className="page__invoice">
       <Navigation />
-      Invoice Balance page is still under contruction
+      <img src="https://images.unsplash.com/photo-1485083269755-a7b559a4fe5e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=869&q=80" /> 
+
     </div>
   );
 };
